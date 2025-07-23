4a21de801f65ac107683f5043141d9f7bda15040
InfantV <rajinfantgk@gmail.com>
2025-07-23 09:02:20 +0530

    new

---
+++ src/com/det/BankNames.java
@@ -0,0 +1,27 @@
+package com.det;
+
+import java.util.Scanner;
+
+public class BankNames{
+
+	public static void main(String[] args) {
+		
+		//for(initialisation;condition;increment/decreament{
+		
+		
+		//1        //2    //4
+		for(int i=0;i<5;i++) {     //i+1
+			System.out.println(i);  //3
+		}
+		
+			
+		}
+	
+	
+	
+		
+		
+		
+	}
+
+
